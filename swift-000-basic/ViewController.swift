1d19f44c269b7fb3ff185a2a4668c83713671097
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-16 17:18:24 -0300

    :gift: finished: IBOutlets / IBAction / Delegate from Storyboard to ViewController

--- swift-000-basic/ViewController.swift
+++ swift-000-basic/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+  
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var label: UILabel!
+  
+  var newUserName: String = ""
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+  
+  @IBAction func changeGreeting(_ sender: Any) {
+    
+    newUserName = self.textField.text ?? ""
+    self.label.text = newUserName
+    
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    
+    if textField == self.textField {
+      textField.resignFirstResponder()
+    }
+    
+    return true
+  }
 
 }
 
